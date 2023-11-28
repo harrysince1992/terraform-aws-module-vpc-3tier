0b6e21af9110790b0b6f6add28be5b49804725c6
Gurpreet Singh <gurpreeteyebrow@gmail.com>
2023-11-28 12:09:40 +0530

    corrected output issue

--- outputs.tf
+++ outputs.tf
@@ -11,9 +11,9 @@ output "present_vpcs" {
 }
 
 output "private_subnet_ids" {
-    value = aws_subnet.private_subnet[*].id
+    value = aws_subnet.private-subnet[*].id
 }
 
 output "public_subnet_ids" {
-    value = aws_subnet.public_subnet[*].id
+    value = aws_subnet.public-subnet[*].id
 }
