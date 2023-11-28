a1021d9e56090c8c04749e7c50faebee4d259207
Gurpreet Singh <gurpreeteyebrow@gmail.com>
2023-11-28 20:31:11 +0530

    disabling private ip allocation for private subnets

--- subnets.tf
+++ subnets.tf
@@ -23,7 +23,6 @@ resource "aws_subnet" "private_subnet" {
     for_each = local.private_az_cidr_map
     vpc_id = aws_vpc.vpc.id
     cidr_block = each.value
-    map_public_ip_on_launch = true
     availability_zone = each.key
     tags = {
         Name = "${var.app}-private-subnet-${each.key}"
